aa13ee4adeac4e9a2f1b25bb5b9823830cff2d6b
emmanuel obarakpo <emmanuelobarakpo@gmail.com>
2022-09-29 20:37:03 +0100

    Revert "Setting up routes"
    This reverts commit b303a91047752d5592fcade87a63a3e641734a82.

--- backend/routes/postRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const {getPosts, createPost, updatePost, deletePost} = require("../controller/postController")
-
-router.route("/").get(getPosts).post(createPost)
-
-router.route("/:id").put(updatePost).delete(deletePost)
-
-module.exports = router
--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const PORT = process.env.port || 3000
-
-const app = express()
-
-app.use("/api/posts", require("./routes/postRoutes"))
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
