c7bbb4bbd6e51c7de1574e33724ff194a8f9adaf
emmanuel obarakpo <emmanuelobarakpo@gmail.com>
2022-10-06 11:42:02 +0100

    controller and routes setup

--- backend/controller/noteController.js
+++ backend/controller/noteController.js
@@ -1,15 +1,24 @@
-const getNotes = (req, res) => {
+const asynHandler = require("express-async-handler")
+
+const getNotes = asynHandler(async(req, res) => {
     res.status(200).json({message: "get notes"})
-}
-const createNote = (req, res) => {
+})
+
+const createNote = asynHandler(async(req, res) => {
+    if(!req.body.note) {
+        res.status(400)
+        throw new Error('Please add a note')
+    }
     res.status(200).json({message: "create notes"})
-}
-const updateNote = (req, res) => {
+})
+
+const updateNote = asynHandler(async(req, res) => {
     res.status(200).json({message: `update note ${req.params.id}`})
-}
-const deleteNote = (req, res) => {
+})
+
+const deleteNote = asynHandler(async(req, res) => {
     res.status(200).json({message: `delete note ${req.params.id}`})
-}
+})
 
 module.exports = {
     getNotes,
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
-require("dotenv").config()
 const express = require("express")
+const dotenv = require("dotenv").config()
+const { errorHandler } = require("./middleware/errorMiddleware")
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 app.use("/api/notes", require("./routes/noteRoutes"))
+app.use(errorHandler)
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
