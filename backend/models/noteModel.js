6597128a2212a9162f3e90fd746ca1f4ac7e188c
emmanuel obarakpo <emmanuelobarakpo@gmail.com>
2022-10-09 23:26:11 +0100

    initial route setup for users

--- backend/models/noteModel.js
+++ backend/models/noteModel.js
@@ -1,16 +1,22 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const noteSchema = mongoose.Schema(
-    {
-        title: {
-            type: String,
-            required: [true, 'Please add a title'],
-        },
-        body: {
-            type: String,
-            required: [true, 'Please add a body'],
-        },
-    }, {timestamps: true,}
-)
+	{
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			ref: "User",
+		},
+		title: {
+			type: String,
+			required: [true, "Please add a title"],
+		},
+		body: {
+			type: String,
+			required: [true, "Please add a body"],
+		},
+	},
+	{ timestamps: true }
+);
 
-module.exports = mongoose.model('Note', noteSchema)
+module.exports = mongoose.model("Note", noteSchema);
--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,10 @@ connectDB()
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
 app.use("/api/notes", require("./routes/noteRoutes"))
+app.use("/api/users", require("./routes/userRoutes"))
+
 app.use(errorHandler)
 
 app.listen(PORT, ()=>{
