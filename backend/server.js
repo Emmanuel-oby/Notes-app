b9bdeedda3b428b469c8f8926c61f79ad31119ee
Emmanuel-oby <emmanuelobarakpo@gmail.com>
2023-04-24 16:57:58 +0100

    gfffgf

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const connectDB = async ()=>{
         if (process.env.NODE_ENV == "production") {
             app.use(express.static("frontend/build"));
             app.get("*", (req, res) => {
-              res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+              res.sendFile(path.resolve("frontend", "build", "index.html"));
             });
         }
 
