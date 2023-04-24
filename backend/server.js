360693ebd83a14566b96c5d28d036549f0dd6cec
Emmanuel-oby <emmanuelobarakpo@gmail.com>
2023-04-24 02:50:48 +0100

    production setup-4

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const colors = require("colors")
 const dotenv = require("dotenv").config()
 const { errorHandler } = require("./middleware/errorMiddleware")
-const {connectDB} = require("./config/db")
 const PORT = process.env.PORT || 3000
 const app = express()
 
