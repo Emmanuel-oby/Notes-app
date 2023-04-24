e73ff374dc2a27937da3f060400bbe3c1963fa22
Emmanuel-oby <emmanuelobarakpo@gmail.com>
2023-04-24 02:17:57 +0100

    production setup-2

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,7 +10,7 @@ const connectDB = async ()=>{
             app.use(express.static("frontend/build"));
             const path = require("path");
             app.get("*", (req, res) => {
-              res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+              res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
             });
         }
 
