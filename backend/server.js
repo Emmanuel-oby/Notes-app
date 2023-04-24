11e5bed49f87a5fe7540128a1a34250e5a60777b
Emmanuel-oby <emmanuelobarakpo@gmail.com>
2023-04-24 15:43:07 +0100

    gggg

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,6 @@ const connectDB = async ()=>{
         console.log(`mongodb connected: ${conn.connection.host}`.cyan.underline);
         if (process.env.NODE_ENV == "production") {
             app.use(express.static("frontend/build"));
-            
             app.get("*", (req, res) => {
               res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
             });
