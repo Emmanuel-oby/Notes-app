0a82c54b2d9d5b3c994191d410e67c830b33b705
Emmanuel-oby <emmanuelobarakpo@gmail.com>
2023-04-24 01:43:45 +0100

    production setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose");
+const express = require("express")
+const app = express()
 
 const connectDB = async ()=>{
     try{
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`mongodb connected: ${conn.connection.host}`.cyan.underline);
+        if (process.env.NODE_ENV == "production") {
+            app.use(express.static("frontend/build"));
+            const path = require("path");
+            app.get("*", (req, res) => {
+              res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+            });
+        }
 
     } catch (error){
         console.log(error);
--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,11 @@ const dotenv = require("dotenv").config()
 const { errorHandler } = require("./middleware/errorMiddleware")
 const {connectDB} = require("./config/db")
 const PORT = process.env.PORT || 3000
+const app = express()
 
 connectDB()
 
-const app = express()
+
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
